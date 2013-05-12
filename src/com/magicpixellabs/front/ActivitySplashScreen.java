8e0b4398261f497c4edfed9380d995edc8aea5b8
Nicholas Rempel <nbrempel@gmail.com>
2013-05-12 15:10:03 -0700

    added custom button assets and created style/added to global theme

--- src/com/magicpixellabs/front/ActivitySplashScreen.java
+++ src/com/magicpixellabs/front/ActivitySplashScreen.java
@@ -12,7 +12,6 @@ public class ActivitySplashScreen extends Activity implements View.OnClickListen
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.Theme_Owl);
         setContentView(R.layout.splash_screen);
         getActionBar().setTitle(null);
 
